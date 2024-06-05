49fc48f1f1ba3a56416fab2d4fd44b0bdb6b6b9f
tahabakuer <80453477+tahabakuer@users.noreply.github.com>
2024-06-05 16:14:25 +0300

    SinglePlayer and Multiplayer
    Game modes seperated

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,13 +5,19 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
 {
-    [SerializeField] private Button playButton;
+    [SerializeField] private Button playSinglePlayerButton;
+    [SerializeField] private Button playMultiPlayerButton;
     [SerializeField] private Button optionsButton;
     [SerializeField] private Button quitButton;
 
     private void Awake()
     {
-        playButton.onClick.AddListener(()=> {
+        playSinglePlayerButton.onClick.AddListener(() => {
+            KitchenGameMultiplayer.playMultiplayer = false;
+            Loader.Load(Loader.Scene.GameScene);
+        });
+        playMultiPlayerButton.onClick.AddListener(()=> {
+            KitchenGameMultiplayer.playMultiplayer = true;
             Loader.Load(Loader.Scene.LobbyScene);
         });
         optionsButton.onClick.AddListener(() => {
